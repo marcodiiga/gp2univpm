4142361f6c815702311e37bc8bf5a999c0715d22
Marco A <marco.diiga@gmail.com>
2015-03-06 10:17:01 +0100

    Added quit logic to last slide, packed a bin and reorganized folders

---
+++ presentation/presentationSrc/Assets/Scripts/changeScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeScene : MonoBehaviour {
+
+	public int levelIndex;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.RightArrow)) {
+			// Next level
+			Application.LoadLevelAsync(levelIndex);
+		}
+	}
+}
---
+++ presentation/presentationSrc/Assets/Scripts/scene1/rotateLogo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateLogo : MonoBehaviour {
+
+	public GameObject logo;
+
+	// Use this for initialization
+	void Start () {
+		QualitySettings.SetQualityLevel(5);
+		Screen.showCursor = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 angles = logo.transform.eulerAngles;
+		angles.y += 1.0f * Time.deltaTime * 20.0f;
+		if(logo.transform.eulerAngles.y > 90.0f && logo.transform.eulerAngles.y < 180) {
+			angles.y += 180;
+		}
+		logo.transform.eulerAngles = angles;
+	}
+}
